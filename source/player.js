fcd9f8b07667149f0b8fc11d11d70cf4e1ae745f
Eric Burns <eric.burns@lmco.com>
2014-04-02 17:42:24 -0400

    Move 3rd person camera out so there are 10 grid squares on either side of rover

--- source/player.js
+++ source/player.js
@@ -1,12 +1,12 @@
 this.togglePerspective = function() {
   var firstPersonTransform = [ -1,  0, 0, 0,  0,    -1,   0,     0,  0,     0, 1,     0,      0, 0,  135, 0 ];
-  var thirdPersonTransform = [  0, -1, 0, 0,  0.707,  0, -0.707, 0,  0.707, 0, 0.707, 0,  -1000, 0, 1000, 0 ];
+  var thirdPersonTransform = [  0, -1, 0, 0,  0.707,  0, -0.707, 0,  0.707, 0, 0.707, 0,  -5800, 0, 5800, 0 ];
 
   if ( this.camera.isFirstPerson ) {
     // Switch to third person
     this.rover.visible = true;
     this.camera.transformTo( thirdPersonTransform, 2 );
-    this.camera.navmode = "fly";
+    this.camera.navmode = "none";
     this.camera.isFirstPerson = false;
   } else {
     // Switch to first person
