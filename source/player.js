4b9e8a3f34fdba6414da48ed608a8f7a5a0df133
Eric Burns <eric.burns@lmco.com>
2014-04-04 13:48:19 -0400

    Add delays to changing rover visibility and navigation mode (for transition time)

--- source/player.js
+++ source/player.js
@@ -1,18 +1,20 @@
 this.togglePerspective = function() {
   var firstPersonTransform = [ -1,  0, 0, 0,  0,    -1,   0,     0,  0,     0, 1,     0,      0, 0,  135, 0 ];
   var thirdPersonTransform = [  0, -1, 0, 0,  0.707,  0, -0.707, 0,  0.707, 0, 0.707, 0,  -5800, 0, 5800, 0 ];
+  var durationSeconds = 1;
+  var delaySeconds = 0.1;
 
   if ( this.camera.isFirstPerson ) {
     // Switch to third person
-    this.rover.visible = true;
-    this.camera.transformTo( thirdPersonTransform, 2 );
+    this.camera.transformTo( thirdPersonTransform, durationSeconds );
+    this.rover.future( delaySeconds ).visible = true;
     this.camera.navmode = "none";
     this.camera.isFirstPerson = false;
   } else {
     // Switch to first person
-    this.rover.visible = false;
-    this.camera.transformTo( firstPersonTransform, 2 );
-    this.camera.navmode = "walk";
+    this.camera.transformTo( firstPersonTransform, durationSeconds );
+    this.rover.future( durationSeconds - delaySeconds ).visible = false;
+    this.camera.future( durationSeconds ).navmode = "walk";
     this.camera.isFirstPerson = true;
   }
 } //@ sourceURL=player
