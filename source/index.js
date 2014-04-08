a92c78d5eee6913b84d76ec807ee6958bde4b66f
Eric Burns <eric.burns@lmco.com>
2014-04-08 17:40:51 -0400

    Remove accidental override of view/threejs' onkeyup function

--- source/index.js
+++ source/index.js
@@ -1,6 +1,10 @@
+var existingOnKeyUp = window.onkeyup;
+
 window.onkeyup = function (event) {
 	switch ( event.keyCode ) {
 		case 80:
 			vwf_view.kernel.callMethod( vwf_view.kernel.find( undefined, "/player" ), "togglePerspective" );
+		default:
+			existingOnKeyUp.call( this, event );
 	}
 } //@ sourceURL=index.js
