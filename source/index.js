1a54a06ba5019b328f4ecb9a59bddd9c97dbd6d6
Eric Burns <eric.burns@lmco.com>
2014-04-09 13:36:42 -0400

    Replace onkeyup hack with addEventListener

--- source/index.js
+++ source/index.js
@@ -1,10 +1,6 @@
-var existingOnKeyUp = window.onkeyup;
-
-window.onkeyup = function (event) {
+window.addEventListener( "keyup", function (event) {
 	switch ( event.keyCode ) {
 		case 80:
 			vwf_view.kernel.callMethod( vwf_view.kernel.find( undefined, "/player" ), "togglePerspective" );
-		default:
-			existingOnKeyUp.call( this, event );
 	}
-} //@ sourceURL=index.js
+} ); //@ sourceURL=index.js
