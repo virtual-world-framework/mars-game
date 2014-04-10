f1b27bcf6e94ac31f3474a6f908c12aac106cc27
Eric Burns <eric.burns@lmco.com>
2014-04-10 17:34:11 -0400

    Factor js out of .html file and into index.js
    Conflicts:
    	source/index.js

--- source/index.js
+++ source/index.js
@@ -1,6 +1,18 @@
+Blockly.inject( document.getElementById( 'blocklyDiv' ), { 
+    path: './source/blockly/', 
+    toolbox: document.getElementById( 'toolbox' ) 
+} );
+
+function onRun() {
+    console.info( "========= onRun =========" );
+    var appId = vwf_view.kernel.find( "", "/" )[ 0 ];
+
+    vwf_view.kernel.setProperty( appId, "executing", true );
+}
+
 window.addEventListener( "keyup", function (event) {
-	switch ( event.keyCode ) {
-		case 80:
-			vwf_view.kernel.callMethod( vwf_view.kernel.find( undefined, "/player" ), "togglePerspective" );
-	}
+    switch ( event.keyCode ) {
+        case 80:
+            vwf_view.kernel.callMethod( vwf_view.kernel.find( undefined, "/player" ), "togglePerspective" );
+    }
 } ); //@ sourceURL=index.js
