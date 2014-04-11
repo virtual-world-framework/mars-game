309148530fe16cdeb9d6445aacaa19d3f522bd30
Eric Burns <eric.burns@lmco.com>
2014-04-11 14:34:10 -0400

    Add new terrain and rover

--- source/player.js
+++ source/player.js
@@ -1,6 +1,6 @@
 this.togglePerspective = function() {
-  var firstPersonTransform = [ -1,  0, 0, 0,  0,    -1,   0,     0,  0,     0, 1,     0,      0, 0,  135, 0 ];
-  var thirdPersonTransform = [  0, -1, 0, 0,  0.707,  0, -0.707, 0,  0.707, 0, 0.707, 0,  -5800, 0, 5800, 0 ];
+  var firstPersonTransform = [ 1, 0, 0, 0,  0,     1,  0,     0,  0, 0,     1,     0,  0,   0,  1, 0 ];
+  var thirdPersonTransform = [ 1, 0, 0, 0,  0, 0.707, -0.707, 0,  0, 0.707, 0.707, 0,  0, -55, 55, 1 ];
   var durationSeconds = 1;
   var delaySeconds = 0.1;
 
