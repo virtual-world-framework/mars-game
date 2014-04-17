3d8a93fc182e52591732790e127973e3801671ad
Scott Haynes <scottnc27603@gmail.com>
2014-04-17 12:45:37 -0400

    Changes to match creating the blackly div in the driver

--- source/index.js
+++ source/index.js
@@ -3,10 +3,7 @@ var HDRShader;
 var hud;
 var frame = 0;
 
-Blockly.inject( document.getElementById( 'blocklyDiv' ), { 
-    path: './source/blockly/', 
-    toolbox: document.getElementById( 'toolbox' ) 
-} );
+
 
 function onRun() {
     vwf_view.kernel.setProperty( vwf_view.kernel.application(), "executing", true );
@@ -38,12 +35,25 @@ vwf_view.firedEvent = function( nodeID, eventName, eventArgs ) {
 
 }
 
+vwf_view.createdNode = function( nodeID, childID, childExtendsID, childImplementsIDs, childSource, childType, childIndex, childName ) {
+
+    if ( childID === vwf_view.kernel.application() ) {
+        console.info( "application created node ==========================" );
+    }
+}
+
 vwf_view.initializedNode = function( nodeID, childID, childExtendsID, childImplementsIDs, childSource, childType, childIndex, childName ) {
 
-    if ( nodeID === vwf_view.kernel.application() ) {
+    if ( childID === vwf_view.kernel.application() ) {
 
+        console.info( "application initialized node ==========================" );
+        
         vwf_view.kernel.kernel.views["vwf/view/threejs"].render = setUp;
 
+        Blockly.inject( document.getElementById( 'blocklyDiv' ), { 
+            path: './source/blockly/', 
+            toolbox: document.getElementById( 'toolbox' ) 
+        } );
     }
 
 }
