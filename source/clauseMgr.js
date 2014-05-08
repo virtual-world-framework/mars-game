41db94fcdc872086508a47f3346f9ebc379da6ea
Kevin Dill <kevin.dill@lmco.com>
2014-05-08 14:28:25 -0400

    convert tabs to spaces.

--- source/clauseMgr.js
+++ source/clauseMgr.js
@@ -3,137 +3,137 @@ var self;
 this.clauseSets$ = [];
 
 this.initialize = function() {
-	self = this;
+    self = this;
 
-	self.addClauseSet(self.defaultClauseSet);
+    self.addClauseSet(self.defaultClauseSet);
 }
 
 this.constructClause = function( clause, callback, context ) {
-	// So this is a little bit ugly (or maybe just Javascript-ey)...
-	// The first thing we're going to do is get the name of the clause, which 
-	//  is done using black magic and the power of the internets, as follows:
-	var clauseKeys = Object.keys( clause );
-	if ( clauseKeys.length != 1 )
-	{
-		this.logger.errorx( "constructClause", "Malformed clause." );
-		return undefined;
-	}
-	var clauseName = clauseKeys[ 0 ];
-   	var clauseParams = clause[ clauseName ];
+    // So this is a little bit ugly (or maybe just Javascript-ey)...
+    // The first thing we're going to do is get the name of the clause, which 
+    //  is done using black magic and the power of the internets, as follows:
+    var clauseKeys = Object.keys( clause );
+    if ( clauseKeys.length != 1 )
+    {
+        this.logger.errorx( "constructClause", "Malformed clause." );
+        return undefined;
+    }
+    var clauseName = clauseKeys[ 0 ];
+    var clauseParams = clause[ clauseName ];
  
-	for ( var i = 0; i < self.clauseSets$.length; ++i ) {
-		var clauseSet = self.clauseSets$[ i ];
+    for ( var i = 0; i < self.clauseSets$.length; ++i ) {
+        var clauseSet = self.clauseSets$[ i ];
 
-		// This gray magic looks on this particular clause set to see if it has the
-		//  required constructor function.
-		var constructorFn = clauseSet[ clauseName ];
+        // This gray magic looks on this particular clause set to see if it has the
+        //  required constructor function.
+        var constructorFn = clauseSet[ clauseName ];
 
-		// If we found it, call it.  The all take the same arguments - clauseParams
-		//  is an array which contains the "real" arguments.
-		if (constructorFn) {
-			return constructorFn( clauseParams, callback, context );
-		}
-	}
+        // If we found it, call it.  The all take the same arguments - clauseParams
+        //  is an array which contains the "real" arguments.
+        if (constructorFn) {
+            return constructorFn( clauseParams, callback, context );
+        }
+    }
 
-	this.logger.errorx( "constructClause", "Failed to create clause of " +
-								 "type '" + clauseName + "'." );
+    this.logger.errorx( "constructClause", "Failed to create clause of " +
+                                 "type '" + clauseName + "'." );
 
-	return undefined;
+    return undefined;
 }
 
 this.addClauseSet = function( clauseSet ) {
-	self.clauseSets$.unshift( clauseSet );
+    self.clauseSets$.unshift( clauseSet );
 }
 
 this.findInContext = function( context, objectName ) {
-	if (!context){
-		this.logger.errorx( "findInContext", "Context  is undefined!" );
-		return undefined;
-	}
+    if (!context){
+        this.logger.errorx( "findInContext", "Context  is undefined!" );
+        return undefined;
+    }
 
-	var results = context.find( "*/" + objectName );
+    var results = context.find( "*/" + objectName );
 
-	if ( results.length < 1 ) {
-		this.logger.errorx( "findInContext", "Object '" + objectName + 
-									 "' not found" );
-		return undefined;
-	}
-	
-	if ( results.length > 1 ) {
-		this.logger.warnx( "findInContext", "Multiple objects named '" + 
-						   			objectName + "' found.  Names should really " +
-						   			"be unique... but we'll return the first one." );
-	} 
+    if ( results.length < 1 ) {
+        this.logger.errorx( "findInContext", "Object '" + objectName + 
+                                     "' not found" );
+        return undefined;
+    }
+    
+    if ( results.length > 1 ) {
+        this.logger.warnx( "findInContext", "Multiple objects named '" + 
+                                    objectName + "' found.  Names should really " +
+                                    "be unique... but we'll return the first one." );
+    } 
 
-	return results[ 0 ];
+    return results[ 0 ];
 }
 
 this.defaultClauseSet.and = function( params, callback, context ) {
-	if ( !params || ( params.length < 1 ) ) {
-		this.logger.errorx( "and", "The 'and' clause needs to have at " +
-									 		" least one (and ideally two or more) clauses " +
-									 		" inside of it." );
-		return undefined;
-	} else if ( params.length < 2 ) {
-		this.logger.warnx( "and", "The 'and' clause probably ought to " +
-						   				   "have two or more clauses inside of it." );
-	}
+    if ( !params || ( params.length < 1 ) ) {
+        this.logger.errorx( "and", "The 'and' clause needs to have at " +
+                                            " least one (and ideally two or more) clauses " +
+                                            " inside of it." );
+        return undefined;
+    } else if ( params.length < 2 ) {
+        this.logger.warnx( "and", "The 'and' clause probably ought to " +
+                                           "have two or more clauses inside of it." );
+    }
 
-	var clauses = [];
-	for ( var i = 0; i < params.length; ++i ) {
-		var thisClause = self.constructClause( params[ i ], callback, context );
-		thisClause && clauses.push( thisClause );
-	}
+    var clauses = [];
+    for ( var i = 0; i < params.length; ++i ) {
+        var thisClause = self.constructClause( params[ i ], callback, context );
+        thisClause && clauses.push( thisClause );
+    }
 
-	return function() {
-		for ( var i = 0; i < clauses.length; ++i ) {
-			if ( !clauses[ i ]() ) { 
-				return false;
-			}
-		}
-		return true;
-	}
+    return function() {
+        for ( var i = 0; i < clauses.length; ++i ) {
+            if ( !clauses[ i ]() ) { 
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 this.defaultClauseSet.or = function( params, callback, context ) {
-	if ( !params || ( params.length < 1 ) ) {
-		this.logger.errorx( "or", "The 'or' clause needs to have at " +
-									 		"least one (and ideally two or more) clauses " +
-									 		"inside of it." );
-		return undefined;
-	} else if ( params.length < 2 ) {
-		this.logger.warnx( "or", "The 'or' clause probably ought to " +
-						   				   "have two or more clauses inside of it." );
-	}
+    if ( !params || ( params.length < 1 ) ) {
+        this.logger.errorx( "or", "The 'or' clause needs to have at " +
+                                            "least one (and ideally two or more) clauses " +
+                                            "inside of it." );
+        return undefined;
+    } else if ( params.length < 2 ) {
+        this.logger.warnx( "or", "The 'or' clause probably ought to " +
+                                           "have two or more clauses inside of it." );
+    }
 
-	var clauses = [];
-	for ( var i = 0; i < params.length; ++i ) {
-		var thisClause = self.constructClause( params[ i ], callback, context );
-		thisClause && clauses.push( thisClause );
-	}
+    var clauses = [];
+    for ( var i = 0; i < params.length; ++i ) {
+        var thisClause = self.constructClause( params[ i ], callback, context );
+        thisClause && clauses.push( thisClause );
+    }
 
    return function() {
-		for ( var i = 0; i < clauses.length; ++i ) {
-			if ( clauses[ i ]() ) { 
-				return true;
-			}
-		}
-		return false;
-	}
+        for ( var i = 0; i < clauses.length; ++i ) {
+            if ( clauses[ i ]() ) { 
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 this.defaultClauseSet.not = function( params, callback, context ) {
-	if ( !params || ( params.length != 1 ) ) {
-		this.logger.errorx( "not", "The 'not' clause needs to have one " +
-									 		"clause inside of it." );
-		return undefined;
-	}
+    if ( !params || ( params.length != 1 ) ) {
+        this.logger.errorx( "not", "The 'not' clause needs to have one " +
+                                            "clause inside of it." );
+        return undefined;
+    }
 
-	var clause = self.constructClause( params[ 0 ], callback, context );
+    var clause = self.constructClause( params[ 0 ], callback, context );
 
-	return function() {
-		return !clause();
-	}
+    return function() {
+        return !clause();
+    }
 }
 
 //@ sourceURL=source/clauseMgr.js
