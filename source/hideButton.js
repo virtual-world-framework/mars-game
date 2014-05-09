290a8f4a17c8e7ad1da0fa38af66c8c423adec50
Eric Burns <eric.burns@lmco.com>
2014-05-09 11:15:22 -0400

    Fix issue where minirover "visible" was stomping on blocklyButton "visible"

--- source/hideButton.js
+++
@@ -1,8 +0,0 @@
-this.initialize = function() {
-	// if anyone want to spend another 8 hours figuring out why ever attempt to hide
-	// this button on start up then be my guess, I give up
-	console.info( "hideButton.initialize ====================================" )
-	console.info( "this.children[ 'blocklyButton' ].visible = " + this.children[ 'blocklyButton' ].visible );
-	this.children[ 'blocklyButton' ].visible = false;
-	console.info( "this.children[ 'blocklyButton' ].visible = " + this.children[ 'blocklyButton' ].visible );
-}
