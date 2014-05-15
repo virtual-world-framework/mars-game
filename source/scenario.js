577eb76f882a69a1629b831edeb4532c8b97af6b
Scott Haynes <scottnc27603@gmail.com>
2014-05-15 09:45:14 -0400

    Clarifies a variable name

--- source/scenario.js
+++ source/scenario.js
@@ -107,14 +107,14 @@ this.startStateParamSet.ram = function( params, context ) {
     }
 
     var objectName = params[ 0 ];
-    var starting = params[ 1 ];
+    var startingRam = params[ 1 ];
     var maxCommands = params[ 2 ];
 
     var object = self.startStateExecutor.findInContext( context, objectName );
 
     // If we want each block to take 1 unit of RAM then these values should
     //  be the same.
-    object.ram = starting;
+    object.ram = startingRam;
     object.ramMax = maxCommands;
     object.blockly_allowedBlocks = maxCommands;
 }
