5abcb686857df40bb356568ebbc5a68c04dc8460
Eric Burns <eric.burns@lmco.com>
2014-05-28 16:16:13 -0400

    Get camera following rover in first person (height and rotation) and refactor

--- source/player.js
+++ source/player.js
@@ -1,10 +1,11 @@
 var self;
-var firstPersonTransform = [ 0 ,-1, 0, 0,  1, 0, 0, 0,  0, 0, 1, 0,  0, 0, 1, 1 ];
-var thirdPersonTransform = [ 
+var thirdPersonOrientationTransform = [ 
       0    , -1,  0    , 0, 
       0.963,  0, -0.27 , 0,
       0.27 ,  0,  0.963, 0,
-    -15.531,  0,  5.553, 1 ];
+      0,      0,  0,     1 ];
+var firstPersonOffset = [ 0, 0, 1 ];
+var thirdPersonOffset = [ -15.531,  0,  5.553 ];
 
 this.initialize = function() {
     self = this;
@@ -13,14 +14,19 @@ this.initialize = function() {
 }
 
 this.initializeEventHandlers = function() {
-    this.rover.moved = function( displacement ) {
-        firstPersonTransform[ 12 ] += displacement[ 0 ];
-        firstPersonTransform[ 13 ] += displacement[ 1 ];
-        firstPersonTransform[ 14 ] += displacement[ 2 ];
-        thirdPersonTransform[ 12 ] += displacement[ 0 ];
-        thirdPersonTransform[ 13 ] += displacement[ 1 ];
-        thirdPersonTransform[ 14 ] += displacement[ 2 ];
-        self.camera.translateBy( displacement, 1 );
+    this.rover.transformChanged = function( transform ) {
+        if ( self.camera.isFirstPerson ) {
+            var firstPersonTransform = transform.slice( 0, 16 );
+            firstPersonTransform[ 12 ] += firstPersonOffset[ 0 ];
+            firstPersonTransform[ 13 ] += firstPersonOffset[ 1 ];
+            firstPersonTransform[ 14 ] += firstPersonOffset[ 2 ];
+            self.camera.transform = firstPersonTransform;
+        } else {
+            self.camera.translation = [ 
+                transform[ 12 ] + thirdPersonOffset[ 0 ],
+                transform[ 13 ] + thirdPersonOffset[ 1 ],
+                transform[ 14 ] + thirdPersonOffset[ 2 ] ];
+        }
     }
 }
 
@@ -30,6 +36,10 @@ this.togglePerspective = function() {
 
     if ( this.camera.isFirstPerson ) {
         // Switch to third person
+        var thirdPersonTransform = thirdPersonOrientationTransform.slice( 0, 16 );
+        thirdPersonTransform[ 12 ] = this.rover.transform[ 12 ] + thirdPersonOffset[ 0 ];
+        thirdPersonTransform[ 13 ] = this.rover.transform[ 13 ] + thirdPersonOffset[ 1 ];
+        thirdPersonTransform[ 14 ] = this.rover.transform[ 14 ] + thirdPersonOffset[ 2 ];
         this.camera.transformTo( thirdPersonTransform, durationSeconds );
         this.blocklyButtonVisible = this.rover.blocklyButton.visible;
         this.future( delaySeconds ).setRoverVisible( true );
@@ -37,6 +47,10 @@ this.togglePerspective = function() {
         this.camera.isFirstPerson = false;
     } else {
         // Switch to first person
+        var firstPersonTransform = this.rover.transform.slice( 0, 16 );
+        firstPersonTransform[ 12 ] += firstPersonOffset[ 0 ];
+        firstPersonTransform[ 13 ] += firstPersonOffset[ 1 ];
+        firstPersonTransform[ 14 ] += firstPersonOffset[ 2 ];
         this.camera.transformTo( firstPersonTransform, durationSeconds );
         this.blocklyButtonVisible = this.rover.blocklyButton.visible;
         this.future( durationSeconds - delaySeconds ).setRoverVisible( false );
