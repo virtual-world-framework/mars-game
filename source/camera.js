b83a4eaad436a16e29e5db448542a7a0924eb3f9
Eric Burns <eric.burns@lmco.com>
2014-05-29 17:53:01 -0400

    Allow camera to have more than two points of view and allow it change its target

---
+++ source/camera.js
@@ -0,0 +1,55 @@
+this.initialize = function() {
+    this.future( 0 ).onSceneReady();
+}
+
+this.onSceneReady = function() {
+    this.updateCamera();
+}
+
+this.updateCamera = function() {
+    var durationSeconds = 1;
+    var targetTransform;
+    var targetNode;
+    if ( this.targetPath ) {
+        targetNode = this.find( this.targetPath )[ 0 ];
+    }
+    if ( targetNode ) {
+        targetTransform = targetNode.transform.slice( 0, 16 );
+    } else {
+        targetTransform = [
+            1, 0, 0, 0,
+            0, 1, 0, 1,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ];
+    }
+    switch ( this.pointOfView ) {
+        case "firstPerson":
+            var firstPersonOffset = [ 0, 0, 1 ];
+            var firstPersonTransform = targetTransform;
+            firstPersonTransform[ 12 ] += firstPersonOffset[ 0 ];
+            firstPersonTransform[ 13 ] += firstPersonOffset[ 1 ];
+            firstPersonTransform[ 14 ] += firstPersonOffset[ 2 ];
+            this.transformTo( firstPersonTransform, durationSeconds );
+            this.future( durationSeconds ).navmode = "walk";
+            break;
+        case "thirdPerson":
+            var thirdPersonOrientationTransform = [ 
+                0    , -1,  0    , 0, 
+                0.963,  0, -0.27 , 0,
+                0.27 ,  0,  0.963, 0,
+                0,      0,  0,     1 ];
+            var thirdPersonOffset = [ -15.531,  0,  5.553 ];
+            var thirdPersonTransform = thirdPersonOrientationTransform.slice( 0, 16 );
+            thirdPersonTransform[ 12 ] = targetTransform[ 12 ] + thirdPersonOffset[ 0 ];
+            thirdPersonTransform[ 13 ] = targetTransform[ 13 ] + thirdPersonOffset[ 1 ];
+            thirdPersonTransform[ 14 ] = targetTransform[ 14 ] + thirdPersonOffset[ 2 ];
+            this.transformTo( thirdPersonTransform, durationSeconds );
+            this.navmode = "none";
+            break;
+        default:
+            break;
+    } 
+}
+
+//@ sourceURL=source/camera.js
--- source/player.js
+++ source/player.js
@@ -1,9 +1,4 @@
 var self;
-var thirdPersonOrientationTransform = [ 
-      0    , -1,  0    , 0, 
-      0.963,  0, -0.27 , 0,
-      0.27 ,  0,  0.963, 0,
-      0,      0,  0,     1 ];
 var firstPersonOffset = [ 0, 0, 1 ];
 var thirdPersonOffset = [ -15.531,  0,  5.553 ];
 
@@ -34,28 +29,16 @@ this.togglePerspective = function() {
     var durationSeconds = 1;
     var delaySeconds = 0.1;
 
-    if ( this.camera.isFirstPerson ) {
+    if ( this.camera.pointOfView === "firstPerson" ) {
         // Switch to third person
-        var thirdPersonTransform = thirdPersonOrientationTransform.slice( 0, 16 );
-        thirdPersonTransform[ 12 ] = this.rover.transform[ 12 ] + thirdPersonOffset[ 0 ];
-        thirdPersonTransform[ 13 ] = this.rover.transform[ 13 ] + thirdPersonOffset[ 1 ];
-        thirdPersonTransform[ 14 ] = this.rover.transform[ 14 ] + thirdPersonOffset[ 2 ];
-        this.camera.transformTo( thirdPersonTransform, durationSeconds );
+        this.camera.pointOfView = "thirdPerson";
         this.blocklyButtonVisible = this.rover.blocklyButton.visible;
         this.future( delaySeconds ).setRoverVisible( true );
-        this.camera.navmode = "none";
-        this.camera.isFirstPerson = false;
     } else {
         // Switch to first person
-        var firstPersonTransform = this.rover.transform.slice( 0, 16 );
-        firstPersonTransform[ 12 ] += firstPersonOffset[ 0 ];
-        firstPersonTransform[ 13 ] += firstPersonOffset[ 1 ];
-        firstPersonTransform[ 14 ] += firstPersonOffset[ 2 ];
-        this.camera.transformTo( firstPersonTransform, durationSeconds );
+        this.camera.pointOfView = "firstPerson";
         this.blocklyButtonVisible = this.rover.blocklyButton.visible;
         this.future( durationSeconds - delaySeconds ).setRoverVisible( false );
-        this.camera.future( durationSeconds ).navmode = "walk";
-        this.camera.isFirstPerson = true;
     }
 } 
 
