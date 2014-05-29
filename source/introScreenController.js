8103226b537bed0fded27eb0c172dc234fdd0c24
Adam Aji <adam.m.aji@lmco.com>
2014-05-29 15:11:38 -0400

    Add placeholder images and center screens

--- source/introScreenController.js
+++ source/introScreenController.js
@@ -6,15 +6,12 @@ var numberOfScreens;
 function setUpIntro() {
     div = document.createElement( 'div' );
     div.id = "introScreen";
-    div.onclick = nextIntroSlide;
-    div.style.height = "256px";
-    div.style.width = "512px";
-    div.style.marginLeft = "-256px";
-    div.style.marginTop = "-128px";
+    div.style.height = "100%";
+    div.style.width = "100%";
     div.style.position = "absolute";
-    div.style.top = "50%";
-    div.style.left = "50%";
-    div.style.backgroundColor = "#424242";
+    div.style.top = "0";
+    div.style.left = "0";
+    div.style.backgroundColor = "#000";
 
     images = [];
     numberOfScreens = 3;
@@ -23,39 +20,47 @@ function setUpIntro() {
     for ( var i = 0; i < numberOfScreens; i++ ) {
         var image = document.createElement( 'img' );
         image.src = dir + "screen" + i + ".png";
-        image.style.display = "block";
-        image.style.marginLeft = "auto";
-        image.style.marginRight = "auto";
-        image.style.marginTop = "15%";
+        image.style.margin = "-384px 0 0 -512px";
+        image.style.top = "50%";
+        image.style.left = "50%";
+        image.style.position = "absolute";
+        image.onclick = nextIntroSlide;
         images.push( image );
     }
 
-
-    index = 0;
-    nextIntroSlide();
-
     document.body.appendChild( div );
+
+    index = -1;
+    nextIntroSlide();
 }
 
 function nextIntroSlide() {
-    if ( index > images.length - 1 ) {
-        if ( div ){
+
+    if ( div ) {
+        index++;
+
+        //If we're at the end of the deck, remove the intro screen div
+        if ( index > images.length - 1 ) {
             document.body.removeChild( div );
             delete div;
         }
-    }
-    else {
-        if ( index != 0 ){
-            div.removeChild( div.firstChild );
+
+        //Otherwise, move to next screen
+        else {
+            if ( index != 0 ){
+                div.removeChild( div.lastChild );
+            }
+            div.appendChild( images[ index ] );
         }
-        div.appendChild( images[ index++ ] );
     }
 }
 
 function prevIntroSlide() {
-    if ( index > 0 ) {
+
+    if ( ( div ) && ( index > 0 ) ) {
+        index--;
         div.removeChild( div.lastChild );
-        div.appendChild( images[ --index ] );
+        div.appendChild( images[ index ] );
     }
 }
 
