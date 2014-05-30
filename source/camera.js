f224af0919f1e1ec017401443623e37975af9cf7
Eric Burns <eric.burns@lmco.com>
2014-05-30 14:40:41 -0400

    Fix issue where every movement of the camera was being done over 1 sec interval

--- source/camera.js
+++ source/camera.js
@@ -1,3 +1,5 @@
+var durationSeconds = 1;
+var delaySeconds = 0.1;
 var self = this;
 var cachedTargetNode;
 
@@ -6,7 +8,29 @@ this.initialize = function() {
 }
 
 this.onSceneReady$ = function() {
-    this.updateCamera$();
+    setTargetEventHandler();
+}
+
+this.changePointOfView$ = function( newPointOfView ) {
+    var previousPointOfView = this.pointOfView;
+    if ( newPointOfView === previousPointOfView ) {
+        return;
+    }
+    this.pointOfView = newPointOfView;
+    this.transformTo( getNewCameraTransform(), durationSeconds );
+    switch ( newPointOfView ) {
+        case "firstPerson":
+            this.future( durationSeconds ).navmode = "walk";
+            break;
+        case "thirdPerson":
+            this.navmode = "none";
+            break;
+        default:
+            self.logger.warnx( "changePointOfView$", "Unrecognized camera point of view: '", 
+                newPointOfView, "'" );
+            break;
+    }
+    manageTargetVisibility();
 }
 
 this.setTargetPath$ = function( newTargetPath ) {
@@ -16,41 +40,51 @@ this.setTargetPath$ = function( newTargetPath ) {
     }
     var previousTargetNode = getTargetNode();
     if ( previousTargetNode ) {
-        previousTargetNode.visible = true;
+        previousTargetNode.future( delaySeconds ).visible = true;
         previousTargetNode.transformChanged = previousTargetNode.events.flush( this );
     }
     this.targetPath = newTargetPath;
     cachedTargetNode = null;
-    this.updateCamera$();
+    this.needToSetupEventHandler$ = true;
+    setTargetEventHandler();
+    this.transformTo( getNewCameraTransform(), durationSeconds );
+    manageTargetVisibility();
 }
 
-this.updateCamera$ = function() {
-    var durationSeconds = 1;
-    var delaySeconds = 0.1;
-    var targetTransform;
-    var targetNode = getTargetNode();
-    if ( targetNode ) {
-        targetTransform = targetNode.transform.slice( 0, 16 );
-    } else {
-        targetTransform = [
-            1, 0, 0, 0,
-            0, 1, 0, 1,
-            0, 0, 1, 0,
-            0, 0, 0, 1
-        ];
+this.followTarget$ = function() {
+    this.transform = getNewCameraTransform();
+}
+
+function getTargetNode() {
+    if ( !cachedTargetNode ) {
+        cachedTargetNode = self.targetPath ? self.find( self.targetPath )[ 0 ] : undefined;
     }
-    switch ( this.pointOfView ) {
+    return cachedTargetNode;
+}
+
+function setTargetEventHandler() {
+    var targetNode = getTargetNode();
+    if ( targetNode && self.needToSetupEventHandler$ ) {
+        targetNode.transformChanged = targetNode.events.add( self.followTarget$, self );
+        self.needToSetupEventHandler$ = false;
+    }
+}
+
+function getNewCameraTransform() {
+    var targetNode = getTargetNode();
+    var targetTransform = targetNode ? targetNode.transform.slice( 0, 16 ) : [
+        1, 0, 0, 0,
+        0, 1, 0, 1,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ];
+    var newCameraTransform;
+    switch ( self.pointOfView ) {
         case "firstPerson":
-            var firstPersonOffset = [ 0, 0, 1 ];
-            var firstPersonTransform = targetTransform;
-            firstPersonTransform[ 12 ] += firstPersonOffset[ 0 ];
-            firstPersonTransform[ 13 ] += firstPersonOffset[ 1 ];
-            firstPersonTransform[ 14 ] += firstPersonOffset[ 2 ];
-            this.transformTo( firstPersonTransform, durationSeconds );
-            this.future( durationSeconds ).navmode = "walk";
-            if ( targetNode ) {
-                targetNode.future( durationSeconds - delaySeconds ).visible = false;
-            }
+            newCameraTransform = targetTransform;
+            newCameraTransform[ 12 ] += self.firstPersonOffset[ 0 ];
+            newCameraTransform[ 13 ] += self.firstPersonOffset[ 1 ];
+            newCameraTransform[ 14 ] += self.firstPersonOffset[ 2 ];
             break;
         case "thirdPerson":
             var thirdPersonOrientationTransform = [ 
@@ -58,57 +92,37 @@ this.updateCamera$ = function() {
                 0.963,  0, -0.27 , 0,
                 0.27 ,  0,  0.963, 0,
                 0,      0,  0,     1 ];
-            var thirdPersonOffset = [ -15.531,  0,  5.553 ];
-            var thirdPersonTransform = thirdPersonOrientationTransform.slice( 0, 16 );
-            thirdPersonTransform[ 12 ] = targetTransform[ 12 ] + thirdPersonOffset[ 0 ];
-            thirdPersonTransform[ 13 ] = targetTransform[ 13 ] + thirdPersonOffset[ 1 ];
-            thirdPersonTransform[ 14 ] = targetTransform[ 14 ] + thirdPersonOffset[ 2 ];
-            this.transformTo( thirdPersonTransform, durationSeconds );
-            this.navmode = "none";
-            if ( targetNode ) {
-                targetNode.future( delaySeconds ).visible = true;
-            }
+            newCameraTransform = thirdPersonOrientationTransform.slice( 0, 16 );
+            newCameraTransform[ 12 ] = targetTransform[ 12 ] + self.thirdPersonOffset[ 0 ];
+            newCameraTransform[ 13 ] = targetTransform[ 13 ] + self.thirdPersonOffset[ 1 ];
+            newCameraTransform[ 14 ] = targetTransform[ 14 ] + self.thirdPersonOffset[ 2 ];
             break;
         default:
+            self.logger.warnx( "getNewCameraTransform", "Unrecognized camera point of view: '", 
+                self.pointOfView, "'" );
+            newCameraTransform = targetTransform;
             break;
-    } 
+    }
+    return newCameraTransform;
 }
 
-function getTargetNode() {
-    if ( !cachedTargetNode ) {
-        cachedTargetNode = self.targetPath ? self.find( self.targetPath )[ 0 ] : undefined;
-        if ( cachedTargetNode ) {
-            cachedTargetNode.transformChanged = cachedTargetNode.events.add( 
-                function( transform ) {
-                    switch ( self.pointOfView ) {
-                        case "firstPerson":
-                            var firstPersonTransform = transform.slice( 0, 16 );
-                            self.firstPersonOffset[ 0 ] = self.firstPersonOffset[ 0 ] || 0;
-                            self.firstPersonOffset[ 1 ] = self.firstPersonOffset[ 1 ] || 0;
-                            self.firstPersonOffset[ 2 ] = self.firstPersonOffset[ 2 ] || 0;
-                            firstPersonTransform[ 12 ] += self.firstPersonOffset[ 0 ];
-                            firstPersonTransform[ 13 ] += self.firstPersonOffset[ 1 ];
-                            firstPersonTransform[ 14 ] += self.firstPersonOffset[ 2 ];
-                            self.transform = firstPersonTransform;
-                            break;
-                        case "thirdPerson":
-                            self.thirdPersonOffset[ 0 ] = self.thirdPersonOffset[ 0 ] || 0;
-                            self.thirdPersonOffset[ 1 ] = self.thirdPersonOffset[ 1 ] || 0;
-                            self.thirdPersonOffset[ 2 ] = self.thirdPersonOffset[ 2 ] || 0;
-                            self.translation = [ 
-                                transform[ 12 ] + self.thirdPersonOffset[ 0 ],
-                                transform[ 13 ] + self.thirdPersonOffset[ 1 ],
-                                transform[ 14 ] + self.thirdPersonOffset[ 2 ] ];
-                            break;
-                        default:
-                            self.warnx( "camera.transformChanged-handler",
-                                "Invalid pointOfView '", self.pointOfView, "'" );
-                            break;
-                    }
-                }, self );
-        }
+function manageTargetVisibility() {
+    var targetNode = getTargetNode();
+    if ( !targetNode ) {
+        return;
     }
-    return cachedTargetNode;
+    switch ( self.pointOfView ) {
+        case "firstPerson":
+            targetNode.future( durationSeconds - delaySeconds ).visible = false;
+            break;
+        case "thirdPerson":
+            targetNode.future( delaySeconds ).visible = true;
+            break;
+        default:
+            self.logger.warnx( "manageTargetVisibility", "Unrecognized camera point of view: '", 
+                self.pointOfView, "'" );
+            break;
+    }
 }
 
 //@ sourceURL=source/camera.js
