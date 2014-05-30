fee7c033ea578d723675228a9e0b2060e13038ac
Scott Haynes <scottnc27603@gmail.com>
2014-05-29 21:16:12 -0400

    Refactors all of the properties of the blockly/manager

--- source/blocklyButton.js
+++ source/blocklyButton.js
@@ -1,6 +1,6 @@
 this.pointerClick = function( pointerInfo, pickInfo ) {
     var scene = this.find( '/' )[ 0 ];
     if ( scene ) {
-        scene.blocklyUiNodeID = this.parent.id;
+        scene.blockly_activeNodeID = this.parent.id;
     }
 }
