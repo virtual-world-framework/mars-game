3ec8c073f360d5c6c1a2ba340a32158f8fb3a388
Eric Burns <eric.burns@lmco.com>
2014-06-02 13:43:08 -0400

    Add comments

--- source/camera.js
+++ source/camera.js
@@ -17,7 +17,11 @@ this.changePointOfView$ = function( newPointOfView ) {
         return;
     }
     this.pointOfView = newPointOfView;
+
+    // Smoothly move the camera to the new point of view
     this.transformTo( getNewCameraTransform(), durationSeconds );
+
+    // Set the navigation mode of the camera appropriately for the new point of view
     switch ( newPointOfView ) {
         case "firstPerson":
             this.future( durationSeconds ).navmode = "walk";
@@ -30,6 +34,9 @@ this.changePointOfView$ = function( newPointOfView ) {
                 newPointOfView, "'" );
             break;
     }
+
+    // Hide the target if the camera is moving into first-person mode
+    // Make it visible if it is in any other mode
     manageTargetVisibility();
 }
 
@@ -38,16 +45,31 @@ this.setTargetPath$ = function( newTargetPath ) {
     if ( newTargetPath === previousTargetPath ) {
         return;
     }
+
+    // Put the previous target node back the way we found it
     var previousTargetNode = getTargetNode();
     if ( previousTargetNode ) {
         previousTargetNode.future( delaySeconds ).visible = true;
         previousTargetNode.transformChanged = previousTargetNode.events.remove( this.followTarget$ );
     }
+
     this.targetPath = newTargetPath;
+
+    // Nullify the cachedTargetNode so getTargetNode will find it fresh next time it is called
     cachedTargetNode = null;
+
+    // Attempt to set an event handler for when the target moves so the camera can follow
+    // If we cannot find the target because the scene has not been fully created, the 
+    // needToSetupEventHandler$ property remains true so that it the event handler will be set in
+    // onSceneReady$
     this.needToSetupEventHandler$ = true;
     setTargetEventHandler();
+
+    // Smoothly move the camera to the new target
     this.transformTo( getNewCameraTransform(), durationSeconds );
+    
+    // Hide the target if the camera is moving into first-person mode
+    // Make it visible if it is in any other mode
     manageTargetVisibility();
 }
 
@@ -81,12 +103,18 @@ function getNewCameraTransform() {
     var newCameraTransform;
     switch ( self.pointOfView ) {
         case "firstPerson":
+            
+            // Have the camera follow the target transform plus a position offset
             newCameraTransform = targetTransform;
             newCameraTransform[ 12 ] += self.firstPersonOffset[ 0 ];
             newCameraTransform[ 13 ] += self.firstPersonOffset[ 1 ];
             newCameraTransform[ 14 ] += self.firstPersonOffset[ 2 ];
             break;
+
         case "thirdPerson":
+            
+            // Lock the camera's orientation, but have it's position follow the target 
+            // (plus an offset)
             var thirdPersonOrientationTransform = [ 
                 0    , -1,  0    , 0, 
                 0.963,  0, -0.27 , 0,
@@ -97,6 +125,7 @@ function getNewCameraTransform() {
             newCameraTransform[ 13 ] = targetTransform[ 13 ] + self.thirdPersonOffset[ 1 ];
             newCameraTransform[ 14 ] = targetTransform[ 14 ] + self.thirdPersonOffset[ 2 ];
             break;
+
         default:
             self.logger.warnx( "getNewCameraTransform", "Unrecognized camera point of view: '", 
                 self.pointOfView, "'" );
@@ -111,6 +140,9 @@ function manageTargetVisibility() {
     if ( !targetNode ) {
         return;
     }
+
+    // Hide the target at the right time if the camera is moving into first-person mode
+    // Immediately make it visible if it is in any other mode
     switch ( self.pointOfView ) {
         case "firstPerson":
             targetNode.future( durationSeconds - delaySeconds ).visible = false;
