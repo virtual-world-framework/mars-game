ca04c99ea08504abe67393647b6287a0ee73035c
Spencer Frazier <spencer.j.frazier@lmco.com>
2014-06-05 16:53:07 -0400

    Completed Loading

--- source/marsGameSound.js
+++ source/marsGameSound.js
@@ -5,19 +5,30 @@ this.initialize = function() {
 
 	self = this;
 
-	var name = this.SoundSet.Sound1.soundName;
-	var url = this.SoundSet.Sound1.soundURL;
-	//this.logger.errorx( "loadSound", num );
-    var soundDefinition = { soundName: name, soundURL: url }
-    var playMe = function() {
-        self.playSound("laser");
-    }
-    this.loadSound(soundDefinition, playMe);
+	this.loadAllSounds();
 
 }
 
 this.loadAllSounds = function() {
-	
+
+	var allSounds = this.SoundSet;
+
+	for (var i in allSounds) { 
+
+   		var name = allSounds[i].soundName;
+		var url = allSounds[i].soundURL;
+		var looping = allSounds[i].isLooping;
+
+   		var soundDefinition = allSounds[i];
+
+    	var playMe = function() {
+      		self.playSound(name);
+    	}
+
+    	this.loadSound(soundDefinition, playMe);
+
+	}
+
 }
 
 
