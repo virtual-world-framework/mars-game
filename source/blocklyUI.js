a36e17c5df966ce616320d1120712bb1ce7e3fb8
Adam Aji <adam.m.aji@lmco.com>
2014-06-06 12:09:51 -0400

    Fix ram bar calling undefined blocklyNode

--- source/blocklyUI.js
+++ source/blocklyUI.js
@@ -1,19 +1,17 @@
-var ramBarCount;
-var ramBar;
+var ramBarCount = document.createElement( "div" );
+var ramBar = document.createElement( "div" );
 
 function setUpBlocklyPeripherals() {
 
     var blocklyHandle = document.createElement( "div" );
     blocklyHandle.id = "blocklyHandle";
-    $( "#blocklyWrapper" ).append( blocklyHandle )
+    $( "#blocklyWrapper" ).append( blocklyHandle );
     $( "#blocklyWrapper" ).draggable( {
         containment: "body",
         handle: "div#blocklyHandle"
     } );
 
-    ramBar = document.createElement( "div" );
     ramBar.id = "ramBar";
-    ramBarCount = document.createElement( "div" );
     ramBarCount.id = "ramBarCount";
     ramBarCount.innerHTML = 15;
     blocklyHandle.appendChild( ramBar );
@@ -21,8 +19,10 @@ function setUpBlocklyPeripherals() {
 }
 
 function updateBlocklyRamBar() {
-    ramBar.style.width = 280 * ( blocklyNodes[ currentBlocklyNodeID ].ram / blocklyNodes[ currentBlocklyNodeID ].ramMax ) + "px";
-    ramBarCount.innerHTML = blocklyNodes[ currentBlocklyNodeID ].ram ;
+    if ( currentBlocklyNodeID ) {
+        ramBar.style.width = 280 * ( blocklyNodes[ currentBlocklyNodeID ].ram / blocklyNodes[ currentBlocklyNodeID ].ramMax ) + "px";
+        ramBarCount.innerHTML = blocklyNodes[ currentBlocklyNodeID ].ram ;
+    }
 }
 
 //@ sourceURL=source/blocklyUI.js
