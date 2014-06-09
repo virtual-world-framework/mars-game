6399737412bf29a1fa35cfc3756b95c36575df64
Kevin Dill <kevin.dill@lmco.com>
2014-06-09 15:57:36 -0400

    Fixed some bugs - passing it back to Spencer.

--- source/marsGameSound.js
+++ source/marsGameSound.js
@@ -1,50 +1,56 @@
+"use strict";
+
+var self;
+
 this.initialize = function() {
+    self = this;
 
-	//self = this;
+    for ( var i = 0; i < this.soundSet.length; ++i ) {
+        this.loadSound(this.soundSet[i]);
+    }
 
-	
-	self = this;
+    // var loadNext = function (i){
+    //  if (self.SoundSet[i] === undefined) {
 
-	var loadNext = function (i){
-		if (self.SoundSet[i] === undefined) {
-
-		}else{
-			self.loadSound(self.SoundSet[i],loadNext(i+1));
-		}
-	}
-    this.loadSound(this.SoundSet[0],loadNext(1));
+    //  }else{
+    //      self.loadSound(self.SoundSet[i],loadNext(i+1));
+    //  }
+    // }
+ //    this.loadSound(this.SoundSet[0],loadNext(1));
 
 }
 
 this.loadAllSounds = function(exitCallback) {
 
-	//Explore generators/promises here to track completion of all loading
+    //Explore generators/promises here to track completion of all loading
 
 
 
-	
+    
 
 }
 
 
 this.playMenuMusic = function(){
-	this.playSound(this.SoundSet[1]);
+    this.playSound("menu");
+    this.playSound(this.SoundSet[1].name);
 }
 
 this.playMainMusic = function(){
-	this.playSound(this.SoundSet[2]);
+    this.playSound("")
+    this.playSound(this.SoundSet[2]);
 }
 
 this.muteMainMusic = function(){
-	
+    
 }
 
 this.playWindSounds = function(){
-	this.playSound(this.SoundSet[3]);
+    this.playSound(this.SoundSet[3]);
 }
 
 this.playRoverSounds = function(){
-	this.playSound(this.SoundSet[0]);
+    this.playSound(this.SoundSet[0]);
 }
 
 this.stopAllSounds = function(){
