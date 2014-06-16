18f7472727862674b46360ed0251e767dd62221f
Adam Aji <adam.m.aji@lmco.com>
2014-06-16 15:32:01 -0400

    Add direction changing for dust

--- source/dustSystem.js
+++ source/dustSystem.js
@@ -1,28 +1,21 @@
 var self;
-var windDirection;
-var maxScalar;
-var minScalar;
 
 this.initialize = function() {
 
     self = this;
-    windDirection = [ 1, 1, 0 ];
-    maxScalar = 0.01;
+    this.counter = 0;
+    this.changeDirection();
     this.future( 0.05 ).update();
 }
 
 this.update = function() {
     this.followTarget();
-
     this.future( 0.05 ).update();
 }
 
 this.changeDirection = function() {
-    windDirection = [ -1, -1, 0 ];
-    for ( var i = 0; i < windDirection.length; i++ ) {
-        self.maxVelocity[ i ] = maxScalar * windDirection[ i ];
-    }
-    this.future( 100 ).changeDirection();
+    self.rotation = [ 0, 0, 1, self.rotation[ 3 ] + 10 ];
+    this.future( 20 ).changeDirection();
 }
 
 this.followTarget = function() {
