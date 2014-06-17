af8ab826ebc7aaf8893cc6e86997002822229178
Adam Aji <adam.m.aji@lmco.com>
2014-06-17 09:50:44 -0400

    Make direction of dust particles static

--- source/dustSystem.js
+++ source/dustSystem.js
@@ -1,40 +1,14 @@
 var self;
-var count;
-var rotation;
-var maxDirectionChange;
 
 this.initialize = function() {
 
     self = this;
-    count = 0;
-    rotation = 0;
-    maxDirectionChange = 30;
-    this.changeDirection();
     this.future( 0.05 ).update();
 }
 
 this.update = function() {
     this.followTarget();
     this.future( 0.05 ).update();
-}
-
-//Rotates system by maxDirectionChange to simulate wind changing
-this.changeDirection = function() {
-
-    rotation++;
-    if ( rotation > 360 ) {
-        rotation = 0;
-    }
-    self.rotation = [ 0, 0, 1, rotation ];
-
-    //Make the rotation look smooth
-    if ( count < maxDirectionChange ) {
-        count++;
-        this.future( 0.05 ).changeDirection();
-    } else {
-        count = 0;
-        this.future( 50 ).changeDirection();
-    }
 }
 
 this.followTarget = function() {
