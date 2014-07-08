d2c4863ce9c0d4cd05aea8670a0e17fc9765e916
Brett Swift <brett.a.swift@knights.ucf.edu>
2014-07-08 18:53:12 -0400

    Start adding grid tiles to editor

---
+++ editor/scene.js
@@ -0,0 +1,70 @@
+this.objCount = 0;
+
+this.initialize = function() {
+    this.camera.transform = [
+        -0.6877578496932983,0.7259325385093689,2.705990027607186e-7,0,
+        -0.6843284964561462,-0.6483430862426758,-0.3336801826953888,0,
+        -0.24222907423973083,-0.2294914275407791,0.9426885843276978,0,
+        100,100.00012969970703,50.0000114440918,1
+    ];
+
+    this.future( 0 ).createGridDisplay( this.grid );
+}
+
+this.createGridDisplay = function( grid ) {
+    var PASSABLE_COLOR = [ 220, 255, 220 ];
+    var IMPASSABLE_COLOR = [ 255, 220, 220 ];
+    var OPACITY = 0.5;
+    var NORMAL = [ 0, 0, 1 ];
+    var ROTATION = 90;
+    var RENDERTOP = false;
+    var SIZE = 0.9;
+    var origin, name, color;
+    var tiles = new Array;
+
+    var offset = new Array(); 
+    offset.push( grid.gridOriginInSpace[ 0 ] / grid.gridSquareLength );
+    offset.push( grid.gridOriginInSpace[ 1 ] / grid.gridSquareLength );
+
+    for ( var x = 0; x < grid.boundaryValues.length; x++ ) {
+
+        for ( var y = 0; y < grid.boundaryValues[ x ].length; y++ ) {
+
+            name = "tile_" + x + "_" + y;
+
+            origin = [
+            offset[ 0 ] + ( x ),
+            offset[ 1 ] + ( y ),
+            0
+            ];
+
+            color = grid.boundaryValues[ x ][ y ] === -1 ? IMPASSABLE_COLOR : PASSABLE_COLOR;
+
+            tiles.push( { 
+                "plane": {
+                "origin": origin,
+                "normal": NORMAL,
+                "rotationAngle": ROTATION,
+                "size": SIZE,
+                "color": color,
+                "opacity": OPACITY,
+                "renderTop": RENDERTOP
+            } } );
+
+        }
+
+    }
+
+    this.graph.graphGroup(
+        this.graph.tileVisible,
+        tiles
+    );
+}
+
+this.removeGridDisplay = function() {
+    var graph = this.graph;
+
+    for ( var obj in graph.children ) {
+        graph.children.delete( graph.children[ obj ] );
+    }
+}
