727f29257722170195e65f4abe32c306c8dbd41d
Brett Swift <brett.a.swift@knights.ucf.edu>
2014-07-25 14:23:50 -0400

    Improved shadows & minimized shadow movement

--- source/lights.js
+++ source/lights.js
@@ -13,7 +13,8 @@ this.setUpEvents = function() {
 }
 
 this.followCamera = function( camTransform ) {
-    var camPos = [ camTransform[ 12 ], camTransform[ 13 ], 0 ];
+    var camPos = [ Math.round( camTransform[ 12 ] ), 
+                   Math.round( camTransform[ 13 ] ), 0 ];
     var offset = this.offsetFromTarget;
     var newPos = [ camPos[ 0 ] + offset[ 0 ],
                    camPos[ 1 ] + offset[ 1 ],
