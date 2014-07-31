68c717af2bf5448d152e35301be095b90ff39aad
Brett Swift <xclutch@cfl.rr.com>
2014-07-31 18:58:25 -0400

    Cleaned up introScreenController.js & inventory.js

--- source/inventory.js
+++ source/inventory.js
@@ -1,70 +1,56 @@
 this.add = function( objectID ) {
-
     var index = this.slots.length;
-
     if ( !validIndex( index, this.capacity ) ) {
         return;
     }
-
     var sceneNode = this.find( "/" )[ 0 ];
     var object = sceneNode.findByID( sceneNode, objectID );
-
     if ( object && object.isInventoriable ) {
         if ( object.parent_ === this ) {
             return;
         }
-
         object.parent_ = this;
-        this.slots[ index ] = object.id;
+        this.slots[ index ] = objectID;
         object.visible = this.inventoryIsVisible;
         object.pickedUp( object.iconSrc, index, this.id );
     }
 }
 
 this.swap = function( index1, index2 ) {
-
-    if ( !( validIndex( index1, this.capacity ) && validIndex( index2, this.capacity ) ) ) {
+    if ( !validIndex( index1, this.capacity || !validIndex( index2, this.capacity ) ) ) {
         return;
     }
-
     var tmp = this.slots[ index1 ];
     this.slots[ index1 ] = this.slots[ index2 ];
     this.slots[ index2 ] = tmp;
-
-} 
+}
 
 this.remove = function( objectID ) {
-
     var sceneNode = this.find( "/" )[ 0 ];
     var object = sceneNode.findByID( sceneNode, objectID );
-
     if ( object && object.parent === this ) {
-
+        var index = this.slots.indexOf( objectID );
+        for ( var i = index; i < this.slots.length - 1; i++ ) {
+            this.slots[ i ] = this.slots[ i + 1 ];
+        }
+        this.slots.length--;
         object.parent_ = this.find( "//pickups" )[ 0 ];
-
-        this.slots.splice( this.slots.indexOf( objectID ), 1 );
         object.visible = true;
         object.dropped();
-
     }
-
 }
 
 this.empty = function() {
     while ( this.slots.length > 0 ) {
         this.remove( this.slots[ 0 ] );
     }
-
 }
 
 function validIndex( index, capacity ) {
-
     if ( isNaN( index ) || index >= capacity || index < 0 ) {
         return false;
     }
-
     return true;
-
 } 
 
 //@ sourceURL=source/inventory.js
