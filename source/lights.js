0d578473a49b916a4fbbd1d3b0616c078e3c0bef
Brett Swift <xclutch@cfl.rr.com>
2014-07-31 19:02:52 -0400

    Reduced GC in lights.js

--- source/lights.js
+++ source/lights.js
@@ -1,3 +1,6 @@
+var cameraPos = new Array( 3 );
+var newPos = new Array( 3 );
+
 this.initialize = function() {
     this.future( 0 ).setUpEvents();
 }
@@ -7,18 +10,20 @@ this.setUpEvents = function() {
     if ( !scene || scene.name !== "application" ) {
         return;
     }
-
     var cam = scene.player.camera;
     cam.transformChanged = cam.events.add( this.followCamera, this );
 }
 
 this.followCamera = function( camTransform ) {
-    var camPos = [ Math.round( camTransform[ 12 ] ), 
-                   Math.round( camTransform[ 13 ] ), 0 ];
+    camPos[ 0 ] = Math.round( camTransform[ 12 ];
+    camPos[ 1 ] = Math.round( camTransform[ 13 ];
+    camPos[ 2 ] = 0;
     var offset = this.offsetFromTarget;
-    var newPos = [ camPos[ 0 ] + offset[ 0 ],
-                   camPos[ 1 ] + offset[ 1 ],
-                   camPos[ 2 ] + offset[ 2 ] ];
+    newPos[ 0 ] = camPos[ 0 ] + offset[ 0 ];
+    newPos[ 1 ] = camPos[ 1 ] + offset[ 1 ];
+    newPos[ 2 ] = camPos[ 2 ] + offset[ 2 ];
     this.target = camPos;
     this.translateTo( newPos );
 }
+
+//@ sourceURL=source/lights.js
