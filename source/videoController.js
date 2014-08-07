d6c8ea72d48f06f17e403f95b806888f05783489
Adam Aji <adam.m.aji@lmco.com>
2014-08-07 13:22:06 -0400

    Add controller to load and play videos
    Conflicts:
    	index.vwf.css
    	source/index.js

---
+++ source/videoController.js
@@ -0,0 +1,55 @@
+var videos = new Array();
+var videoID = 0;
+
+function loadVideo( src, type ) {
+    var video = {
+        "id" : videoID,
+        "elem" : document.createElement( "video" ),
+        "source" : document.createElement( "source" ),
+        "wrapper" : document.createElement( "div" )
+    };
+    video.elem.appendChild( video.source );
+    video.wrapper.appendChild( video.elem );
+    video.source.src = "assets/video/" + src;
+    video.source.type = type || 'video/mp4;codecs="avc1.42E01E,mp4a.40.2"';
+    video.elem.id = "video" + video.id;
+    video.elem.className = "video";
+    video.wrapper.className = "videoWrapper";
+    video.elem.load();
+
+    video.elem.onclick = removeVideoOnEvent;
+    video.elem.onended = removeVideoOnEvent;
+
+    videos.push( video );
+    videoID++;
+    return videoID - 1;
+}
+
+function playVideo( id ) {
+    var video = videos[ id ];
+    if ( video ) {
+        document.body.appendChild( video.wrapper );
+        video.elem.play();
+    }
+}
+
+function removeVideoOnEvent( event ) {
+    var videoElem = event.srcElement;
+    var id = parseInt( videoElem.id.split( "video" )[ 1 ] );
+    removeVideo( id );
+}
+
+function removeVideo( id ) {
+    var video = videos[ id ];
+    if ( video && video.wrapper.parentNode === document.body ) {
+        $( "#transitionScreen" ).fadeIn( function() {
+            if ( video.id === introVideoId ) {
+                vwf_view.kernel.fireEvent( vwf_view.kernel.application(), "introScreensComplete" );
+            }
+            document.body.removeChild( video.wrapper );
+            $( "#transitionScreen" ).fadeOut( "slow" );
+        } );
+    }
+}
+
+//@ sourceURL=source/videoController.js
