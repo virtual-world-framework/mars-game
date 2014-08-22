f201eb24c5a0427eb5bc4796c0e7f595f1faa11a
Brett Swift <xclutch@cfl.rr.com>
2014-08-21 20:09:59 -0400

    Fix for #3971: Objects visible under ramp

--- source/griddable.js
+++ source/griddable.js
@@ -1,8 +1,12 @@
 this.addedToGrid = function() {
+	// TODO: Find a better way to determine if the object's parent
+	//   is an inventory and if the object is a pickup
     if ( this.parent.remove ) {
         this.parent.remove( this.id );
     }
-    this.visisble = true;
+    if ( this.setPickupVisibility ) {
+    	this.visible = true;
+    }
 }
 
 //@ sourceURL=source/griddable.js
