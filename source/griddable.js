4b14a5dd9e9d182fd36a706a2f50f178c8395922
Brett Swift <xclutch@cfl.rr.com>
2014-08-22 11:34:23 -0400

    Fixed indentation.

--- source/griddable.js
+++ source/griddable.js
@@ -1,11 +1,11 @@
 this.addedToGrid = function() {
-	// TODO: Find a better way to determine if the object's parent
-	//   is an inventory and if the object is a pickup
+    // TODO: Find a better way to determine if the object's parent
+    //   is an inventory and if the object is a pickup
     if ( this.parent.remove ) {
         this.parent.remove( this.id );
     }
     if ( this.setPickupVisibility ) {
-    	this.visible = true;
+        this.visible = true;
     }
 }
 
