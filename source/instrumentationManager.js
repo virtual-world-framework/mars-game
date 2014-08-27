d6a6d491300f6f659283f763ffae34b6503ce6ce
Spencer Frazier <ambientosx@gmail.com>
2014-08-27 12:14:37 -0400

    Add support for player state tracking

--- source/instrumentationManager.js
+++ source/instrumentationManager.js
@@ -1,66 +1,111 @@
+var self;
+
 this.initialize = function() {
-	
-	if ( this.enabled === true ) {
-		this.future( 0 ).registerGameStartedListener();
-		this.future( 0 ).registerScenarioSucceededListener();
-		this.future( 0 ).registerScenarioFailedListener();
-		this.future( 0 ).registerScenarioResetListener();
+    
+    self = this;
+    
+    if ( this.enabled === true ) {
+        this.future( 0 ).registerGameStartedListener();
+        this.future( 0 ).registerScenarioStartedListener();
+        this.future( 0 ).registerScenarioSucceededListener();
+        this.future( 0 ).registerScenarioFailedListener();
+        this.future( 0 ).registerScenarioResetListener();
     }
 }
 
 this.registerGameStartedListener = function() {
-	var scene = this.find( "/" )[ 0 ];
+    var scene = this.find( "/" )[ 0 ];
     scene.gameStarted = ( function( ) {
         this.broadcastEvent( 'gameStarted', '' );
     } ).bind( this );
 }
 
+this.registerScenarioStartedListener = function() {
+    var scene = this.find( "/" )[ 0 ];
+    scene.scenarioStarted = ( function( ) {
+        this.broadcastEvent( 'scenarioStarted', scene.activeScenarioPath );
+    } ).bind( this );
+}
+
 this.registerScenarioSucceededListener = function() {
-	var scene = this.find( "/" )[ 0 ];
+    var scene = this.find( "/" )[ 0 ];
     scene.scenarioSucceeded = ( function( ) {
-    	this.broadcastEvent( 'scenarioSucceeded', scene.activeScenarioPath );
+        this.broadcastEvent( 'scenarioSucceeded', scene.activeScenarioPath );
     } ).bind( this );
 }
 
 this.registerScenarioFailedListener = function() {
-	var scene = this.find( "/" )[ 0 ];
+    var scene = this.find( "/" )[ 0 ];
     scene.scenarioFailed = ( function( ) {
         this.broadcastEvent( 'scenarioFailed', scene.activeScenarioPath );
     } ).bind( this );
 }
 
 this.registerScenarioResetListener = function() {
-	var scene = this.find( "/" )[ 0 ];
+    var scene = this.find( "/" )[ 0 ];
     scene.scenarioReset = ( function( scenarioName ) {
         this.broadcastEvent( 'scenarioReset', scenarioName );
     } ).bind( this );
 }
 
 this.broadcastEvent = function( event, value ) {
-	var params = [ event, value ];
-	this.createRequest ( 'event', params );
+    var params = [ e, v ];
+    this.createRequest ( 'logEvent', params );
 }
 
 this.createRequest = function( type, params ) {
-	
-	var scene = this.find( "/" )[ 0 ];
-	
-	var event = params[ 0 ];
-	var value = params[ 1 ];
-	
-	var playerId = scene.playerId;
-	var version = scene.version;
-	
-	//HACK HACK HACK: Should use vwf utility.resolveURI("/")
-	var pathArray = window.location.pathname.split( '/' );
-	var vwfSession = pathArray[pathArray.length-2];
-	
-    if ( type === 'event' ) {
+    
+    var scene = this.find( "/" )[ 0 ];
+    
+    var playerId = scene.playerId;
+    var version = scene.version;
+    
+    var pathArray = window.location.pathname.split( '/' );
+    var vwfSession = pathArray[pathArray.length-2];
+    
+    var xhr = new XMLHttpRequest();
+            
+    if ( type === 'logEvent' ) {
+        if ( !params || ( params.length !== 2 ) ) {
+        self.logger.warnx( "createRequest", "The logEvent request takes 2 parameters: the" +
+                           " event name and a value associated with the event." );
+        }
+        var event = params[ 0 ];
+        var value = params[ 1 ];
+        
+        xhr.open("POST", this.logEventUrl, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send("vwf_session="+vwfSession+"&player_id="+playerId+"&action="+event+"$&value="+value+"$&version="+version);
+        
+    }
+}
+
+this.getRequest = function( type, params ) {
+    var scene = this.find( "/" )[ 0 ];
+    
+    var playerId = scene.playerId;
+    var version = scene.version;
+    
+    var pathArray = window.location.pathname.split( '/' );
+    var vwfSession = pathArray[pathArray.length-2];
+    
+    var xhr = new XMLHttpRequest();
+    
+    if ( type == 'getPlayerState' ) {
+        if ( params && ( params.length > 0 ) ) {
+        self.logger.warnx( "getRequest", "The getPlayerState request takes no parameters." );
+        }
         var xhr = new XMLHttpRequest();
-		xhr.open("POST", this.url, true);
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.send("vwf_session="+vwfSession+"&player_id="+playerId+"&action="+event+"$&value="+value+"$&version="+version);
-	}
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                return xhr.responseText;
+            }
+        }
+        
+        xhr.open("GET", this.getPlayerStateUrl, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send("player_id="+playerId);
+    }
     
 }
 
