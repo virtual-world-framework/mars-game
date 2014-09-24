1f7b692e3db309221410eace1135dbd0b2b77eaa
Brett Swift <xclutch@cfl.rr.com>
2014-09-24 13:06:30 -0400

    Improved features

--- fileManager.js
+++ fileManager.js
@@ -12,28 +12,97 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 
-var FileManager = function() {
-	if ( File && FileReader && FileList && Blob ) {
-		this.initialize();
-	} else {
-		alert( "Your browser does not support the File APIs!" );
-	}
-	return this;
+var FileManager = function( domParent ) {
+    if ( File && FileReader && FileList && Blob ) {
+        this.initialize( domParent );
+    } else {
+        alert( "Your browser does not support the File APIs!" );
+    }
+    return this;
 }
 
 FileManager.prototype = {
-	constructor: FileManager,
-	cache: undefined,
-	reader: undefined,
-	initialize: function() {
-		this.cache = new Array();
-		this.reader = new FileReader();
-	},
-	readFile: function( file ) {
-		this.reader.onload = function( event ) {
-			console.log( this.reader.result );
-		}
-		this.reader.readAsText( file, "text/plain" );
-	},
-	saveFile: function() {}
-}
+    constructor: FileManager,
+    cache: undefined,
+    reader: undefined,
+    fileInput: undefined,
+    file: undefined,
+    saveLink: undefined,
+    initialize: function( domParent ) {
+        this.cache = {};
+        this.reader = new FileReader();
+        this.fileInput = document.createElement( "input" );
+        this.fileInput.id = "fileInput";
+        this.fileInput.type = "file";
+        this.fileInput.onchange = this.openFile.bind( this );
+        if ( domParent ) {
+            domParent.appendChild( this.fileInput );
+        }
+        this.saveLink = document.createElement( "a" );
+        this.saveLink.id = "saveLink";
+        this.saveLink.style.display = "none";
+        document.body.appendChild( this.saveLink );
+    },
+    openFile: function( callback ) {
+        var file = this.fileInput.files[ 0 ];
+        this.reader.onload = ( function( event ) {
+            var file = this.makeFile( this.reader.result );
+            this.file = file;
+        } ).bind( this );
+        this.reader.readAsText( file, "text/plain" );
+    },
+    makeFile: function( input ) {
+        var file;
+        if ( !( input instanceof Array ) ) {
+            input = [ input ];
+        }
+        file = new Blob( input, { "type": "text/plain" } );
+        return file;
+    },
+    readFile: function( value, callback ) {
+        var file = this.checkCache( value );
+        if ( file === false ) {
+            return;
+        }
+        var contents;
+        this.reader.onload = ( function( event ) {
+            contents = this.reader.result;
+            if ( callback instanceof Function ) {
+                callback( contents );
+            } else {
+                console.log( contents );
+            }
+        } ).bind( this );
+        this.reader.readAsText( file );
+    },
+    saveFile: function( value, filename ) {
+        var file = this.checkCache( value );
+        if ( file === false ) {
+            return;
+        }
+        var url = URL.createObjectURL( file );
+        this.saveLink.innerHTML = "Click to download " + filename;
+        this.saveLink.href = url;
+        this.saveLink.download = filename;
+    },
+    cacheFile: function( file, cacheID ) {
+        this.cache[ cacheID ] = file;
+    },
+    checkCache: function( value ) {
+        if ( value instanceof Blob ) {
+            return value;
+        } else if ( typeof value === "string" ) {
+            if ( this.cache[ value ] instanceof Blob ) {
+                return this.cache[ value ];
+            } else {
+                console.log( "Could not find file with ID " +
+                    "\"" + value + "\" in the cache." );
+                return false;
+            }
+        } else {
+            console.log( "This function only accepts values " +
+                "of the types string and Blob (files)." );
+            return false;
+        }
+    }
+} //@ sourceURL=fileManager.js
