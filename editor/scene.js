0e29ef48e77906727738b3badd5385cc0faa4a1e
Brett Swift <xclutch@cfl.rr.com>
2014-09-30 14:03:47 -0400

    Start adding scenario generation code

--- editor/scene.js
+++ editor/scene.js
@@ -1,3 +1,17 @@
+// Copyright 2014 Lockheed Martin Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may 
+// not use this file except in compliance with the License. You may obtain 
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+
 this.objCount = 0;
 this.selectedObject;
 this.createdObjects = new Array();
@@ -27,7 +41,6 @@ this.initialize = function() {
 
 this.onSceneReady = function() {
     this.setUpListeners();
-    // this.createGridDisplay( this.grid );
 }
 
 this.setUpListeners = function() {
