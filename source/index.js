aec4b3a049da9b4e231ec3ab91795cfe4539aec6
Spencer Frazier <spencer.j.frazier@lmco.com>
2014-12-18 14:34:38 -0500

    Add trigger enabled check to onEvent - currently failing and succeeding

--- source/index.js
+++ source/index.js
@@ -141,6 +141,7 @@ vwf_view.firedEvent = function( nodeID, eventName, eventArgs ) {
                     setRenderMode( RENDER_GAME );
                 }
                 lastBlockIDExecuted = undefined;
+                gridBounds = eventArgs[ 1 ] || gridBounds;
                 enableAllHUDElements();
             case "scenarioReset":
                 removePopup();
