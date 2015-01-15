eb4936c57cfded268ddd9237826be2f304819132
Brett Swift <xclutch@cfl.rr.com>
2015-01-15 16:51:46 -0500

    Add menu to model and track application state

---
+++ source/mainMenu.js
@@ -0,0 +1,23 @@
+// Copyright 2014 Lockheed Martin Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may 
+// not use this file except in compliance with the License. You may obtain 
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+
+this.animate = function() {
+	this.mars.future( 0.05 ).rotatePlanet();
+	this.satellite.future( 0.05 ).moveSatellite();
+	this.scene.gameCam.setCameraTarget( this.target );
+	this.scene.soundManager.playSound( "musicMenuIntro" );
+	this.scene.soundManager.playSound( "musicMenuLoop" );
+}
+
+//@ sourceURL=source/mainMenu.js
