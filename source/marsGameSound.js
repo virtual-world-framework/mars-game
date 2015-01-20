ec18d0653f8a0d504174efd3c7fe5492ea91750e
Nathan Marshak <nathan.x.marshak@lmco.com>
2015-01-19 19:22:10 -0500

    Now meSpeak functionality has been offloaded to marsGameSound.js. This depends on commit 1603ebc1fb8205a807d3fa062cdcad4708b1e872 in vwf (on the mars-game-staging branch).

--- source/marsGameSound.js
+++ source/marsGameSound.js
@@ -16,11 +16,18 @@
 
 this.initialize = function() {
     //Load sounds defined in yaml file
-    this.setVoiceSet( this.voiceSet );
-    var currSound;
-    for( currSound in this.soundSet ) {
-         this.loadSound( this.soundSet[currSound] );
+    this.setVoiceSet( this.voiceSet ); //TODO: get rid of this
+    var currSoundName;
+    for( currSoundName in this.soundSet ) {
+        var currSound = this.soundSet[currSoundName];
+        if( !isTTSSound(currSound) ){
+            this.loadSound( currSound );
+        }
     }
+
+    //TODO: intialize meSpeak... 
+    meSpeak.loadConfig("mespeak/mespeak_config.json");
+    meSpeak.loadVoice("mespeak/en.json");
 
     this.future( 0 ).setUpSubtitles();
 }
@@ -36,14 +43,54 @@ this.setUpSubtitles = function() {
 
 this.playSoundWrapper = function( soundName, exitCallback ){
     var currSound = this.soundSet[soundName];
-    if( currSound.voice && currSound.textToSpeechInput ){
+    if( isTTSSound(currSound) ){
         console.log("Playing TTS sound");
-        this.playSound( soundName, exitCallback );
+        
+
+        var speechStr = currSound.textToSpeechInput;
+        if(speechStr){
+            //var speechStr = rawSubtitle.replace(/\[.*\]: /, ""); //Get rid of "[Rover]: ", "[MC]:", etc.
+            var currVoice = currSound.voice;
+            var meSpeakOpts = {};
+
+            //TODO: there might be a way to assign this.voiceSet[currVoice] directly to meSpeakOpts, which would
+            //make the code cleaner.
+            if ( this.voiceSet[currVoice].ttsAmplitude !== undefined ) {
+                meSpeakOpts.amplitude = this.voiceSet[currVoice].ttsAmplitude;
+            } 
+            if ( this.voiceSet[currVoice].ttsVariant !== undefined ) {
+                meSpeakOpts.variant = this.voiceSet[currVoice].ttsVariant;
+            } 
+            if ( this.voiceSet[currVoice].ttsWordGap !== undefined ) {
+                meSpeakOpts.wordgap = this.voiceSet[currVoice].ttsWordGap;
+            } 
+            if ( this.voiceSet[currVoice].ttsSpeed !== undefined ) {
+                meSpeakOpts.speed = this.voiceSet[currVoice].ttsSpeed;
+            } 
+            if ( this.voiceSet[currVoice].ttsPitch !== undefined ) {
+                meSpeakOpts.pitch = this.voiceSet[currVoice].ttsPitch;
+            } 
+            
+            meSpeakOpts.rawdata = 'default';
+            var meSpeakBuf = meSpeak.speak( speechStr, meSpeakOpts );
+
+            currSound.playOnLoad = true; //no need to call playSound()
+            this.loadSound( currSound, undefined, undefined, meSpeakBuf ); 
+        } else {
+            //TODO: Um... throw a warning? We aren't supposed to be in this state...
+        }
+
+        //TODO: pass the buffer as an extra option to playsound.
+        
     } else {
         console.log("Playing normal sound");
         this.playSound( soundName, exitCallback );
     }
     
+}
+
+function isTTSSound( soundDefinition ){
+    return soundDefinition.voice && soundDefinition.textToSpeechInput;
 }
 
 function startSubtitle( instanceHandle ) {
@@ -74,6 +121,5 @@ function stopSubtitle( instanceHandle ) {
         scene.hideCommsImage();
     }
 }
-
 
 //@ sourceURL=source/marsGameSound.js
