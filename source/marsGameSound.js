9b840282aaab2702d6d666505d4f672ce07a19e9
Brett Swift <xclutch@cfl.rr.com>
2015-02-02 15:42:35 -0500

    Fix event listener syntax for replication

--- source/marsGameSound.js
+++ source/marsGameSound.js
@@ -27,8 +27,8 @@ this.setUpSubtitles = function() {
     var scene = this.find( "/" )[ 0 ];
     if ( scene && this.setUpOnce ) {
         this.setUpOnce = false;
-        this.soundStarted = this.events.add( startSubtitle.bind( this ) );
-        this.soundFinished = this.events.add( stopSubtitle.bind( this ) );
+        this.soundStarted = this.events.add( startSubtitle, this );
+        this.soundFinished = this.events.add( stopSubtitle, this );
     }
 }
 
