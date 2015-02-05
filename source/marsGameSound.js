30b021362f4ecf2d3e5361787b010a2661418450
Spencer Frazier <spencer.j.frazier@lmco.com>
2015-02-05 15:12:46 -0500

    Fixes sound this issues

--- source/marsGameSound.js
+++ source/marsGameSound.js
@@ -27,8 +27,8 @@ this.setUpSubtitles = function() {
     var scene = this.find( "/" )[ 0 ];
     if ( scene && this.setUpOnce ) {
         this.setUpOnce = false;
-        this.soundStarted = this.events.add( function() { startSubtitle.call( this ); }, this );
-        this.soundFinished = this.events.add( function() { stopSubtitle.call( this ); }, this );
+        this.soundStarted = this.events.add( function( instanceHandle ) { startSubtitle.call( this, instanceHandle ); }, this );
+        this.soundFinished = this.events.add( function( instanceHandle ) { stopSubtitle.call( this, instanceHandle ); }, this );
     }
 }
 
