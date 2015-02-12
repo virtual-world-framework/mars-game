36797e0f77191ff584c7731dd9533b8c5ad78489
Nathan Marshak <nathan.x.marshak@lmco.com>
2015-02-12 18:36:31 -0500

    Now player size and container size are set dynamically.

--- source/view/videoController.js
+++ source/view/videoController.js
@@ -15,44 +15,20 @@
 var videos = new Array();
 var videoID = 0;
 var playingVideo;
+var mediaManagerID;
+var videoManagerID;
 
-// vwf_view.firedEvent = function( nodeID, eventName, eventArgs ) {
-//     switch ( eventName ) {
-        
-//     }
-// }
-
-// function loadVideo( src, type, dontRemoveWhenEnded ) {
 function loadVideo( src, type ) {
     var video = {
         "id" : videoID,
-        // "elem" : document.createElement( "video" ),
-        // "source" : document.createElement( "source" ),
-        // "wrapper" : document.createElement( "div" )
     };
-    // video.elem.appendChild( video.source );
-    // video.wrapper.appendChild( video.elem );
     video.source = "assets/video/" + src;
     video.vidName = src;
-    // video.source.type = type || 'video/mp4;codecs="avc1.42E01E,mp4a.40.2"';
-    // video.elem.id = "video" + video.id;
-    // video.elem.className = "video";
-    // video.wrapper.className = "videoWrapper";
-    // video.elem.load();
 
     document.onkeypress = removeVideoOnEvent;
 
-    //Right now the code seems to depend on the removeVideoOnEvent... otherwise
-    //we don't advance when a video finishes, unless the user presses a key.
-    // if ( !dontRemoveWhenEnded ) {
-        // video.elem.onended = removeVideoOnEvent;
-    // }
-
-    // $("#jquery_jplayer_1").bind($.jPlayer.event.ended, removeVideoOnEvent );
-    var mediaManagerID = vwf_view.kernel.find( undefined, "/mediaManager" )[ 0 ];
-    var videoManagerID = vwf_view.kernel.find( mediaManagerID, "videoManager" ) [ 0 ];
-    // var setEndedCallbackInput = [removeVideoOnEvent];
-    // vwf_view.kernel.callMethod( videoManagerID, "setEndedCallback", setEndedCallbackInput );
+    mediaManagerID = vwf_view.kernel.find( undefined, "/mediaManager" )[ 0 ];
+    videoManagerID = vwf_view.kernel.find( mediaManagerID, "videoManager" ) [ 0 ];
     videos.push( video );
     videoID++;
     return videoID - 1;
@@ -61,47 +37,19 @@ function loadVideo( src, type ) {
 }
 
 function playVideo( id ) {
-    // var video = videos[ id ];
-    // if ( video ) {
-    //     document.body.appendChild( video.wrapper );
-    //     playingVideo = video;
-    //     video.elem.play();
-    // }
-
-    //TODO: 
-    //a) Find the mediaManager.
-    //b) Find the videoManager
-    //c) Identify the video source from above (video.source.src)
-    //d) use vwf_view_kernel.setProperty( videoManagerID, "url", <insert_url_here>) to set the URL for the video manager.
-    //e) vwf_view.kernel.callMethod( videoManagerID, "play" );
-
     var video = videos[ id ];
     if ( video ) {
-        var mediaManagerID = vwf_view.kernel.find( undefined, "/mediaManager" )[ 0 ];
-        var videoManagerID = vwf_view.kernel.find( mediaManagerID, "videoManager" ) [ 0 ];
 
         playingVideo = video;
-        // $("#jp_container_1").css('z-index', 103); 
         vwf_view.kernel.setProperty( videoManagerID, "z_index", 103 );
         vwf_view.kernel.callMethod( videoManagerID, "show" );
-        // vwf_view.kernel.callMethod( videoManagerID, "testFireEvent" );
-        // $("#jp_container_1").css('width', 1024);
-        // $("#jp_container_1").css('height', 768);
-        // $("#jquery_jplayer_1").css('width', 1024);
-        // $("#jquery_jplayer_1").css('height', 768);
 
-        //TODO: Is there a better way to do this than to put "/mars-game/" in front of video.source.src ??? 
-        // vwf_view.kernel.setProperty( videoManagerID, "url", '/mars-game/' + video.source.src );
-        // vwf_view.kernel.setProperty( videoManagerID, "url", '/mars-game/assets/video/intro_cinematic.mp4' );
+        //TODO: Is there a better way to do this than to put "/mars-game/" in front of video.source.src ?
         var appName = "mars-game";
         var redactedURL = ( video.source ).replace( new RegExp(appName + "/.*/assets"), appName + "/assets");
         vwf_view.kernel.setProperty( videoManagerID, "url", redactedURL );
         vwf_view.kernel.callMethod( videoManagerID, "play" );
     }
-
-    // var fooRover = vwf_view.kernel.find( "", "//rover" )[ 0 ];
-    // var fooGameCam = vwf_view.kernel.find( "", "//gameCam" )[ 0 ];
-    // console.log("Playing video: " + id);
 }
 
 function removeVideoOnEvent( event ) {
@@ -114,32 +62,16 @@ function removeVideoOnEvent( event ) {
     var videoManagerID = vwf_view.kernel.find( mediaManagerID, "videoManager" ) [ 0 ];
 
     vwf_view.kernel.callMethod( videoManagerID, "clearMedia" );
-    // $("#jp_container_1").css('z-index', -2); //HACK: We really shouldn't depend on a z-index adjustment to make this work.
-    // var videoElem = playingVideo.elem || event.srcElement;
-    if( playingVideo ){ //TODO: Figure out why this check is necessary. 
+    if( playingVideo ){
         var id = playingVideo.id;
         var fileName = videos[id].vidName;
         vwf_view.kernel.fireEvent( vwf_view.kernel.application(), "videoPlayed", [ fileName ] );
         playingVideo = undefined;
     }
-
-    // vwf_view.kernel.fireEvent( vwf_view.kernel.application(), "videoPlayed", undefined );
 }
 
 function removeVideo( id ) {
-    var mediaManagerID = vwf_view.kernel.find( undefined, "/mediaManager" )[ 0 ];
-    var videoManagerID = vwf_view.kernel.find( mediaManagerID, "videoManager" ) [ 0 ];
     vwf_view.kernel.callMethod( videoManagerID, "hide" );
-    // $("#jquery_jplayer_1").css('width', 0);
-    // $("#jquery_jplayer_1").css('height', 0);
-    // $("#jp_container_1").css('width', 0);
-    // $("#jp_container_1").css('height', 0);
-    // $("#jp_container_1").hide();
-    // $("#jquery_jplayer_1").hide();
-    // var video = videos[ id ];
-    // if ( video && video.wrapper.parentNode === document.body ) {
-    //     document.body.removeChild( video.wrapper );
-    // }
 }
 
 function getVideoIdFromSrc( src ) {
@@ -156,12 +88,4 @@ function getVideoFileName( video ) {
     var fileName = video.source.split( "/" ).pop();
     return fileName;
 }
-
-// vwf_view.firedEvent = function( nodeID, eventName, eventArgs ) {
-//     switch ( eventName ) {
-//         case "videoEnded":
-//             console.log("Video Ended zomg!");
-//             break;
-//     }
-// }
 //@ sourceURL=source/videoController.js
