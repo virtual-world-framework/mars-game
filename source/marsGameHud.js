f54bf555e1ad626b14e50535373c9807e6c40ce2
Brett Swift <xclutch@cfl.rr.com>
2015-02-18 16:57:55 -0500

    Add more HUD elements and functionality

--- source/marsGameHud.js
+++ source/marsGameHud.js
@@ -21,4 +21,37 @@ this.optionsButton.onClick = function() {
     hud.scene.pauseGame();
 }
 
+this.comms.addCharacterImage = function( path ) {
+    this.images.characterImage.src = path;
+    this.interval = 0;
+    this.direction = 1;
+}
+
+this.comms.removeCharacterImage = function() {
+    this.interval = 1;
+    this.direction = -1;
+}
+
+this.firstPersonButton.onClick = function() {
+    var hud = this.parent;
+    var camera = hud.scene.gameCam;
+    hud.cameraSelector.activeMode = this.mode;
+    camera.setCameraMount( this.mode );
+}
+
+this.thirdPersonButton.onClick = function() {
+    var hud = this.parent;
+    var camera = hud.scene.gameCam;
+    hud.cameraSelector.activeMode = this.mode;
+    camera.setCameraMount( this.mode );
+}
+
+this.topDownButton.onClick = function() {
+    var hud = this.parent;
+    var camera = hud.scene.gameCam;
+    hud.cameraSelector.activeMode = this.mode;
+    camera.setCameraMount( this.mode );
+    hud.scene.toggledHelicam();
+}
+
 //@ sourceURL=source/marsGameHud.js
