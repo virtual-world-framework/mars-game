52564cd224999d52fb974c52c61943fbe282bb5e
Nathan Marshak <nathan.x.marshak@lmco.com>
2015-02-20 15:21:09 -0500

    Fixed spacing, removed unnecessary transform set, got rid of commented-out code.

--- source/rover.js
+++ source/rover.js
@@ -18,6 +18,9 @@ this.initialize = function() {
         return;
     }
 
+    // TODO: Find current grid square (rather than making app developer specify)
+    // TODO: Find the current heading (rather than making app developer specify)
+
     this.calcRam();
 }
 
@@ -98,66 +101,15 @@ this.placeOnTerrain = function( pos ) {
         pos[ 2 ] - this.transform[ 14 ] 
     ];
     var terrainPosCentroid = this.getTerrainPosUnderNode( this, deltaPos );
-    // var terrainPosFL = this.getTerrainPosUnderNode( this.wheelFL, deltaPos );
-    // var terrainPosBL = this.getTerrainPosUnderNode( this.wheelBL, deltaPos );
-    // var terrainPosFR = this.getTerrainPosUnderNode( this.wheelFR, deltaPos );
-    // var terrainPosBR = this.getTerrainPosUnderNode( this.wheelBR, deltaPos );
-
-    // // Step 2: Find the normal of the plane on which the rover will sit
-    // var vecFLtoFR = goog.vec.Vec3.subtract( terrainPosFR, terrainPosFL, goog.vec.Vec3.create() );
-    // var vecFLtoBL = goog.vec.Vec3.subtract( terrainPosBL, terrainPosFL, goog.vec.Vec3.create() );
-    // var vecBRtoFR = goog.vec.Vec3.subtract( terrainPosFR, terrainPosBR, goog.vec.Vec3.create() );
-    // var vecBRtoBL = goog.vec.Vec3.subtract( terrainPosBL, terrainPosBR, goog.vec.Vec3.create() );
-    // var normal = this.calcUpwardNormalizedPlaneNormal( vecFLtoBL, vecFLtoFR );
-    // var normalEstimate2 = this.calcUpwardNormalizedPlaneNormal( vecBRtoFR, vecBRtoBL );
-    // normal[ 0 ] = ( normal[ 0 ] + normalEstimate2[ 0 ] ) * 0.5;
-    // normal[ 1 ] = ( normal[ 1 ] + normalEstimate2[ 1 ] ) * 0.5;
-    // normal[ 2 ] = ( normal[ 2 ] + normalEstimate2[ 2 ] ) * 0.5;
-
-    // // Step 3: Calculate the x and y axes of the rover in its parent's frame of reference
-    
-    // // Calculate xAxis as y cross z
-    // var yAxis = [ this.transform[ 4 ], this.transform[ 5 ], this.transform[ 6 ] ];
-    // var xAxis = goog.vec.Vec3.cross( yAxis, normal, goog.vec.Vec3.create() );
-
-    // // Normalize x and z axes
-    // // (we do this now so their cross product will be unit length and we'll only have to do it 
-    // // twice instead of three times)
-    // goog.vec.Vec3.normalize( xAxis, xAxis );
-    // goog.vec.Vec3.normalize( normal, normal );
-
-    // // Calculate yAxis as z cross x
-    // goog.vec.Vec3.cross( normal, xAxis, yAxis );
-
-    // // Step 4: Find the point on the terrain below the rover
-    // pos[ 2 ] = ( terrainPosFL[ 2 ] + terrainPosBL[ 2 ] + terrainPosFR[ 2 ] + terrainPosBR[ 2 ] ) * 0.25
 
     pos[ 2 ] = terrainPosCentroid[ 2 ];
 
-    // // Step 5: Assign the new transform to the rover
-    // this.transform = [
-    //     xAxis[ 0 ],  xAxis[ 1 ],  xAxis[ 2 ],  0,
-    //     yAxis[ 0 ],  yAxis[ 1 ],  yAxis[ 2 ],  0,
-    //     normal[ 0 ], normal[ 1 ], normal[ 2 ], 0,
-    //     pos[ 0 ],    pos[ 1 ],    pos[ 2 ],    1
-    // ];
-    // this.transform = [
-    //     1,  0,  0,  0,
-    //     0,  1,  0,  0,
-    //     0, 0, 1, 0,
-    //     pos[ 0 ],    pos[ 1 ],    pos[ 2 ],    1
-    // ];
     this.transform = [
-        this.transform[0],  this.transform[1],  this.transform[2],  0,
-        this.transform[4],  this.transform[5],  this.transform[6],  0,
-        this.transform[8], this.transform[9], this.transform[10], 0,
+        this.transform[ 0 ],  this.transform[ 1 ],  this.transform[ 2 ],  0,
+        this.transform[ 4 ],  this.transform[ 5 ],  this.transform[ 6 ],  0,
+        this.transform[ 8 ], this.transform[ 9 ], this.transform[ 10 ], 0,
         pos[ 0 ],    pos[ 1 ],    pos[ 2 ],    1
     ];
-    
-    // this.transform[12] = pos[0];
-    // this.transform[13] = pos[1];
-    // this.transform[14] = pos[2];
-    // this.transform[15] = 1;
 }
 
 this.translateOnTerrain = function( translation, duration, boundaryValue ) {
