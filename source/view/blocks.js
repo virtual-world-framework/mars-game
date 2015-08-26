4a67edc210c2cc556c7b7d54c977f1325f20d4f9
Spencer Frazier <spencer.j.frazier@lmco.com>
2015-08-26 16:57:29 -0400

    Fix nanite fix.

--- source/view/blocks.js
+++ source/view/blocks.js
@@ -2183,7 +2183,8 @@ Blockly.JavaScript['draw_triangle'] = function(block) {
   //moveA
   //end
 
-  var overallCode = moveA + start + mark + moveB + mark + moveC + mark + moveA + mark + end;
+  //var overallCode = moveA + start + mark + moveB + mark + moveC + mark + moveA + mark + end;
+  var overallCode = start + moveA + mark + moveB + mark + moveC + mark + moveA + mark + end;
   //var overallCode = start + moveA + moveB + moveC + moveA + end;
   
   return overallCode;
