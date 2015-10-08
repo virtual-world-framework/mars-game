3f0e8643816673acf6fab8e67b43ad4d9b563900
Kevin Dill <kevin.dill@lmco.com>
2015-10-08 18:17:03 -0400

    Built starting programs for all m4 tasks, plus other tweaks.

--- source/view/blocks.js
+++ source/view/blocks.js
@@ -4999,8 +4999,7 @@ Blockly.Blocks[ 'math_number_out' ] = {
   init: function() {
     this.setColour( 60 );
     this.appendValueInput( "INPUT" )
-        .appendField(new Blockly.FieldDropdown([["25", "25"],["24", "24"],
-         ["23", "23"],["22", "22"],["21", "21"],["20", "20"],["19", "19"],["18", "18"],["17", "17"],["16", "16"],
+        .appendField(new Blockly.FieldDropdown([["20", "20"],["19", "19"],["18", "18"],["17", "17"],["16", "16"],
          ["15", "15"],["14", "14"],["13", "13"],["12", "12"],["11", "11"],["10", "10"],["9", "9"],["8", "8"],
          ["7", "7"],["6", "6"],["5", "5"],["4", "4"],["3", "3"],["2", "2"],["1", "1"],["0", "0"]]), "VALUE")
         .setCheck( [ 'OperatorAddSubtract','OperatorMultiplyDivide','Variable','LeftParenthesis','RightParenthesis','Conditional' ] );
